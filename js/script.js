5777dfc4048ef944f1ed6012be6c8d6bd18db0b0
Eskin Nikita <ladeuxmaster@gmail.com>
2019-04-18 21:57:25 +0300

    Add scores counter

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let randX;
 let randY;
 let count = 20;
+let scores = 0;
 let fishes = [];
 const fishType = ["fish1", "fish2", "fish3"];
 
 window.onload = () => {
   let aquarium = document.getElementById("aquarium");
-  let counterDisplay = document.getElementById("counter");
   for (let i = 0; i < count; i++) {
     generateFish();
   }
@@ -18,26 +18,50 @@ window.onload = () => {
 
 function generateFish() {
   let type = Math.floor(Math.random() * fishType.length);
+  let randX = getRandom(10, document.documentElement.clientWidth - 100);
+  let randY = getRandom(10, document.documentElement.clientHeight - 100);
   let fish = document.createElement("img");
   fish.setAttribute("src", "../img/" + fishType[type] + ".png");
   fish.setAttribute("width", "80");
   fish.classList.add("fish");
   aquarium.appendChild(fish);
+  fish.style.top = randY + "px";
+  fish.style.left = randX + "px";
+  addScoreHandle(fish);
 }
 
 function moveFish() {
   for (let i = 0; i < fishes.length; i++) {
-    let move = Math.random() * (3000 - 500) + 500;
+    let move = Math.random() * (4000 - 1000) + 1000;
     setTimeout(() => {
-      randX = Math.random() * aquarium.offsetWidth;
-      randY = Math.random() * (aquarium.offsetHeight - 200);
-      if (parseInt(fishes[i].style.left.replace('px', ''), 10) > randX) {
-        fishes[i].style.transform = "scale(-1, 1)";
+      randX = getRandom(10, document.documentElement.clientWidth - 100);
+      randY = getRandom(10, document.documentElement.clientHeight - 100);
+      if (parseInt(fishes[i].style.left.replace("px", ""), 10) > randX) {
       } else {
-        fishes[i].style.transform = "scale(1, 1)";
       }
+      fishes[i].style.transitionDuration = "3s";
       fishes[i].style.top = randY + "px";
       fishes[i].style.left = randX + "px";
     }, move);
+  }
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log();
+function addScoreHandle(fish) {
+  let counterDisplay = document.getElementById("counter");
+  fish.addEventListener("click", () => {
+    scores++;
+    counterDisplay.innerHTML = scores;
+    removeFish()
+  });
+}
+
+function removeFish() {
+  for(let i = 0; i < fishes.length; i++) {
+    fishes.splice(i, 1);
   }
 }
